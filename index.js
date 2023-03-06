3c76c7bd5386c70a39079f2c145af955669860d8
Havok <rngdhavok@hotmail.com>
2023-03-06 18:30:22 -0500

    cleaning up current code

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-
-
 // Connect to database
 const db = mysql.createConnection(
     {
