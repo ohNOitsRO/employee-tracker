c851d6832fc097b94fd8218665335485b5046c9c
Havok <rngdhavok@hotmail.com>
2023-03-09 20:17:07 -0500

    on delete set null fix

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE role (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
-  FOREIGN KEY (dept_id) 
+  FOREIGN KEY (department_id) 
   REFERENCES department(id) ON DELETE SET NULL
 
 );
