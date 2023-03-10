446c3bb149879cea6422cf4691aba5b9fd6792d5
Havok <rngdhavok@hotmail.com>
2023-03-09 20:21:52 -0500

    fixing syntax errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,19 +3,19 @@ VALUES (01, "Billing"),
        (02, "Human Resources"),
        (03, "Customer Experience"),
        (04, "Information Technology"),
-       (05, "Administration")
+       (05, "Administration");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Manager", 95000, 101)
-       ("Senior Lead", 65000, 102)
-       ("Engineer", 80000, 103)
-       ("Customer Liaison", 50000, 104)
-       ("Receptionist", 40000, 105)
+VALUES ("Manager", 95000, 101),
+       ("Senior Lead", 65000, 102),
+       ("Engineer", 80000, 103),
+       ("Customer Liaison", 50000, 104),
+       ("Receptionist", 40000, 105);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("White", "Walter", 1375, 11)
-       ("Pinkman", "Jesse", 1667, 14)
-       ("Schrader", "Hank", 1889, 16)
-       ("Fring", "Gus", 1717, 12)
-       ("Goodman", "Saul", 1315, 19)
+VALUES ("White", "Walter", 1375, 11),
+       ("Pinkman", "Jesse", 1667, 14),
+       ("Schrader", "Hank", 1889, 16),
+       ("Fring", "Gus", 1717, 12),
+       ("Goodman", "Saul", 1315, 19);
        
